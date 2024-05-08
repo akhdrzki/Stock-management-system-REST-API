8883bf895e30a5914422546934cd487404fa1029
akhdrzki <akhadirizky@gmail.com>
2024-05-08 13:11:27 +0800

    init env variable to db connection config

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,10 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "tracksystem",
+  host: Bun.env.DB_HOST,
+  user: Bun.env.DB_USER,
+  password: Bun.env.DB_PASS,
+  database: Bun.env.DB_NAME,
 });
 
 const db = drizzle(pool);
